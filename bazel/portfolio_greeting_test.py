37db45fbeb9c3ae771dcc29ec686b94a198184aa
vparisahi <pari.sahi2025@gmail.com>
2025-12-10 19:49:04 -0800

    Update portfolio_greeting_test.py

--- bazel/portfolio_greeting_test.py
+++ bazel/portfolio_greeting_test.py
@@ -1,7 +1,12 @@
+import sys
+import os
+
+# Make sure the current directory is on the path
+sys.path.append(os.path.dirname(__file__))
+
 from portfolio_greeting import get_message
 
 
 def test_message_runs():
-    # Just make sure we can call the function without error.
     msg = get_message()
     assert isinstance(msg, str)
